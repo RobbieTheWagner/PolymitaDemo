79ff250e8ff865663a9f0f7ff678beb311b5048e
Robert Wagner <rwwagner@vt.edu>
2013-08-28 13:36:50 -0400

    removed target

--- target/jboss-eap-6.1/standalone/deployments/business-central.war/console.config.js
+++
@@ -1,9 +0,0 @@
-var consoleConfig = {
-  profileName:          "jBPM Console",
-  logo:                 "app/jbpm_logo.png",
-  serverWebContext:     "/gwt-console-server",
-  defaultEditor:        "org.jboss.bpm.console.client.process.ProcessModule",
-  overallReportFile:   "overall_activity.rptdesign",
-  processSummaryReportFile: "process_summary.rptdesign",
-  instanceSummaryReportFile:   "instance_summary.rptdesign"
-};
--- target/jboss-eap-6.1/standalone/deployments/jboss-brms.war/index.jsp
+++
@@ -1,4 +0,0 @@
-<%
-    String redirectURL = "org.drools.guvnor.Guvnor/Guvnor.jsp";
-    response.sendRedirect(redirectURL);
-%>
